b64b58fdd48ab2f86e13b0507417fc47607d71f9
GadMinz <baykov97@mail.ru>
2022-10-12 14:08:48 +0300

    Add display data in profile page

--- src/App.tsx
+++ src/App.tsx
@@ -8,18 +8,21 @@ import { fetchUsers } from "./redux/slices/usersSlice";
 const App: React.FC = () => {
   const dispatch = useAppDispatch();
   const { category } = useAppSelector((state) => state.filters);
+  const { items } = useAppSelector((state) => state.users);
   const getUsers = async () => {
     dispatch(fetchUsers(category));
   };
   React.useEffect(() => {
     getUsers();
   }, [category]);
-
+  const getUserById = (id: string) => {
+    return items.filter((item) => item.id === id);
+  };
   return (
     <div className="wrapper">
       <Routes>
         <Route path="/" element={<Main getUsers={getUsers} />} />
-        <Route path="/:id" element={<Profile />} />
+        <Route path="/:id" element={<Profile getUserById={getUserById} />} />
       </Routes>
     </div>
   );
--- src/pages/Profile/Profile.tsx
+++ src/pages/Profile/Profile.tsx
@@ -1,44 +1,78 @@
 import React from "react";
 import s from "./Profile.module.scss";
+import { UserItem } from "../../redux/types";
+import moment from "moment";
+import "moment/locale/ru";
+import { Link, useLocation } from "react-router-dom";
 import GlobalSvgSelector from "../../assets/icons/GlobalSvgSelector";
-import { Link } from "react-router-dom";
+import { categoriesMap } from "../Main/compinents/Header/Categories/Categories";
 
-interface ProfileProps {}
-
-const Profile: React.FC<ProfileProps> = () => {
-  return (
-    <div className={s.profile}>
-      <div className={s.profile_header}>
-        <Link to={"/"} className={s.profile_header_back}>
-          <GlobalSvgSelector id="back" />
-        </Link>
-        <div className={s.profile_header_user}>
-          <div className={s.profile_header_user_avatar}>
-            <img
-              src="https://cloudflare-ipfs.com/ipfs/Qmd3W5DuhgHirLHGVixi6V76LhCkZUz6pnFt5AJBiyvHye/avatar/1210.jpg"
-              alt="profile-img"
-            />
+interface ProfileProps {
+  getUserById: (id: string) => UserItem[];
+}
+moment.locale("ru");
+const Profile: React.FC<ProfileProps> = ({ getUserById }) => {
+  const id = useLocation().pathname.slice(1);
+  const user = getUserById(id);
+  if (user.length > 0) {
+    const {
+      avatarUrl,
+      firstName,
+      lastName,
+      userTag,
+      department,
+      birthday,
+      phone,
+    } = user[0];
+    const date = moment(birthday).format("DD MMMM YYYY");
+    const years = moment().diff(birthday, "years", false);
+    const phoneFormat =
+      phone.slice(0, 2) +
+      " (" +
+      phone.slice(2, 5) +
+      ") " +
+      phone.slice(5, 8) +
+      " " +
+      phone.slice(8, 10) +
+      " " +
+      phone.slice(10, 12);
+    return (
+      <div className={s.profile}>
+        <div className={s.profile_header}>
+          <Link to={"/"} className={s.profile_header_back}>
+            <GlobalSvgSelector id="back" />
+          </Link>
+          <div className={s.profile_header_user}>
+            <div className={s.profile_header_user_avatar}>
+              <img src={avatarUrl} alt="" />
+            </div>
+            <div className={s.profile_header_user_title}>
+              {firstName} {lastName} <p>{userTag}</p>
+            </div>
+            <div className={s.profile_header_user_department}>
+              {categoriesMap[department]}
+            </div>
           </div>
-          <div className={s.profile_header_user_title}>
-            Alice Ivanova <p>ai</p>
+        </div>
+        <div className={s.profile_info}>
+          <div className={s.profile_info_age}>
+            <div className={s.profile_info_age_date}>
+              <GlobalSvgSelector id="star" />
+              {date}
+            </div>
+            <div className={s.profile_info_age_years}>
+              {moment.duration(years, "year").humanize()}
+            </div>
           </div>
-          <div className={s.profile_header_user_department}>Designer</div>
+          <div className={s.profile_info_phone}>
+            <GlobalSvgSelector id="phone" />
+            <a href={`tel: ${phone}`}>{phoneFormat}</a>
+          </div>
         </div>
       </div>
-      <div className={s.profile_info}>
-        <div className={s.profile_info_age}>
-          <div className={s.profile_info_age_date}>
-            <GlobalSvgSelector id="star" />5 июня 1996
-          </div>
-          <div className={s.profile_info_age_years}>24 года</div>
-        </div>
-        <div className={s.profile_info_phone}>
-          <GlobalSvgSelector id="phone" />
-          <a href={`tel: +79515454587`}>+79515454587</a>
-        </div>
-      </div>
-    </div>
-  );
+    );
+  }
+  return <div></div>;
 };
 
 export default Profile;
